077683e2ab16df721ae8efd79601902d5c0a4e64
danyilbutov <89708817+HJyup@users.noreply.github.com>
2026-05-17 20:44:07 +0100

    Add: design-hashmap - submission-3

---
+++ Data Structures & Algorithms/design-hashmap/submission-3.py
@@ -0,0 +1,99 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.capacity = 16
+        self.count = 0
+        self.LOAD_FACTOR = 0.7
+
+        # None - empty
+        self.arr = [None] * self.capacity
+        # hashed key to store when we can keep probing
+        self.tombstones = set()
+
+
+    def _hash(self, key, idx):
+        fst = key % self.capacity
+        snd = 1 + (key % (self.capacity - 1))
+
+        return (fst + idx * snd) % self.capacity
+
+    def resize(self):
+        self.capacity *= 2
+        self.tombstones = set()
+        self.count = 0
+
+        prev_arr = self.arr.copy()
+        self.arr = [None] * self.capacity
+
+        for item in prev_arr:
+            if item is None:
+                continue
+
+            self.put(item[0], item[1])
+
+
+    def put(self, key: int, value: int) -> None:
+        curr = 0
+
+        while curr < self.capacity:
+            hashed = self._hash(key, curr)
+
+            if self.arr[hashed] is None:
+                self.arr[hashed] = (key, value)
+                self.tombstones.discard(hashed)
+
+                self.count += 1
+                if self.count / self.capacity > self.LOAD_FACTOR:
+                    self.resize()
+
+                return None
+
+            if self.arr[hashed][0] == key:
+                self.arr[hashed] = (key, value)
+                
+                return None
+
+            curr += 1
+
+    def get(self, key: int) -> int:
+        curr = 0
+
+        while curr < self.capacity:
+            hashed = self._hash(key, curr)
+            
+            # To continue do probbing
+            if self.arr[hashed] is None and hashed not in self.tombstones:
+                return -1
+
+            if self.arr[hashed] is not None and self.arr[hashed][0] == key:
+                return self.arr[hashed][1]
+
+            curr += 1
+
+        return -1
+
+    def remove(self, key: int) -> None:
+        curr = 0
+
+        while curr < self.capacity:
+            hashed = self._hash(key, curr)
+
+            if self.arr[hashed] is None and hashed not in self.tombstones:
+                return None
+
+            if self.arr[hashed] is not None and self.arr[hashed][0] == key:
+                self.arr[hashed] = None
+                self.count -= 1
+                self.tombstones.add(hashed)
+
+                return None
+
+            curr += 1
+
+        return None
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
